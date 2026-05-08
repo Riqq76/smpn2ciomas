aaf474338ea0058557ac97d08544dcee68d2c2f0
thorikkun <mraero2001@gmail.com>
2026-05-08 15:59:27 +0700

    update project

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "smartschool");
+$conn = mysqli_connect("localhost", "root", "", "smpnciom_smartschool");
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
